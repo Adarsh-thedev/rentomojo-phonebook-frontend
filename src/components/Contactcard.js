bef744979c29759fc6dfa00ec12e746ba75bc7db
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-29 08:52:20 +0530

    changes in trnsition

--- src/components/Contactcard.js
+++ src/components/Contactcard.js
@@ -6,9 +6,9 @@ const Contactcard = (props) => {
     return(
         <div className = 'container mt2'>
             <div className = 'row'>
-                <div className = 'col-sm-10 offset-sm-1 bg1 text-center br-pill shadow-3'>
+                <div className = 'grow col-sm-10 offset-sm-1 bg1 text-center br-pill shadow-3'>
                     <p className = 'f5'>Name : {name}</p>
-                    <span className = 'f4 red'>&#9993;</span><p className = 'f5 dib pr3'>Email : {email}</p>
+                    <span className = 'f4 red'>&#9993;</span><p className = 'f5 dib pr2'>Email : {email}</p>
                     <span className = 'f4 red'>&#9742;</span><p className = 'f5 dib'>Phone : {phoneNumber}</p>
                 </div>
             </div>
